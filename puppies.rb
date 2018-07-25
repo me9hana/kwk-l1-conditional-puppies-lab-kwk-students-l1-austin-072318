ca594c047c9e0e219ce44b5f2353d8a415c48159
me9hana <meghanagoli@gmail.com>
2018-07-25 17:53:50 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -23,7 +23,7 @@ less_puppies(4)
 
 # 3. We've decided to give Rachel a little more control over her puppy obsession. Write a method called some_puppies that takes in two arguments - the number of puppies she has found and a maximum puppy allowance. This method should tell Rachel good job if she gets less than half her maximum allowance and to get back on her puppy restriction when she exceeds her maximum.
 def some_puppies(maxpups, pups1)
-  if pups1 =< maxpups/2 
+  if pups1 <= maxpups/2 
     puts "Good job, Rachel!"
   else 
     puts "Get back on your puppy restriction"
